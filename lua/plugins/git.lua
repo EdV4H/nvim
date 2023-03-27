563d5a6b81a863c1111e2226177667cf8e869c5e
EdV4H <circlemountain.ym@gmail.com>
2023-03-28 02:11:34 +0900

    🔧 add: git blame

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,1 +1,14 @@
-return { "dinhhuy258/git.nvim" }
+return {
+  "dinhhuy258/git.nvim",
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        current_line_blame = true,
+        current_line_blame_opts = {
+          delay = 300,
+        },
+      })
+    end,
+  },
+}
