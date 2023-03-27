ddd3e4393cb126122347fec38c3b5b840c42d6c1
EdV4H <circlemountain.ym@gmail.com>
2023-03-28 00:07:03 +0900

    :wrench: add: keymaps

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("base")
 require("options")
+require("keymaps")
 require("lazy-config")
 require("color-scheme")
 
---
+++ lua/keymaps.lua
@@ -0,0 +1,34 @@
+local opts = { noremap = ture, silent = ture }
+local term_ops = { silent = true }
+
+-- local keymap = vim.keymap
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--  normal_mode = 'n'
+--  insert_mode = 'i',
+--  visual_mode = 'v',
+--  visual_block_mode = 'x',
+--  term_mode = 't',
+--  command_mode = 'c',
+
+-- Normal --
+
+-- Split window
+keymap("n", "ss", ":split<Return><C-w>w", opts)
+keymap("n", "sv", ":vsplit<Return><C-w>w", opts)
+
+-- window navigation
+keymap("n", "sh", "<C-w>h", opts)
+keymap("n", "sk", "<C-w>k", opts)
+keymap("n", "sj", "<C-w>j", opts)
+keymap("n", "sl", "<C-w>l", opts)
+
+-- nohlsearch on ESC*2
+keymap("n", "<ESC><ESC>", ":<C-u>set nohlsearch<Return>", opts)
+
