69418ebc6be7ab15a19d0d21f0b45a9b0eaa64f8
EdV4H <circlemountain.ym@gmail.com>
2023-04-02 16:54:04 +0900

    :wrench: add: indent keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -22,6 +22,10 @@ vim.g.maplocalleader = " "
 -- ';' alternate ':'
 keymap("n", ";", ":", opts)
 
+-- do not exit visual mode on indent
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
 -- Split window
 keymap("n", "ss", ":split<Return><C-w>w", opts)
 keymap("n", "sv", ":vsplit<Return><C-w>w", opts)
